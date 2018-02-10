0dd072a85f39311c5367a6d224b6ab14fd96068f
John Turner <git@norgg.org>
2018-02-10 14:55:15 +0000

    teabag

---
+++ Assets/TeabagString.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeabagString : MonoBehaviour {
+
+	public GameObject bag;
+	public GameObject tag;
+	LineRenderer line;
+
+	void Start() {
+		line = GetComponent<LineRenderer>();
+	}
+	
+	void Update() {
+		line.SetPosition(0, bag.transform.position + bag.transform.forward * transform.localScale.y );
+		line.SetPosition(1, tag.transform.position - tag.transform.forward * transform.localScale.y / 3);
+	}
+}
